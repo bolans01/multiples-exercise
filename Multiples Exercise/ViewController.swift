4e15c5adc401313d5f269bd7d7bbb93a3019c67b
bolans01 <bolans01@gmail.com>
2015-09-29 22:21:13 -0400

    Label can now update

--- Multiples Exercise/ViewController.swift
+++ Multiples Exercise/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userInputAmount = 0
+    var calculatedMultiple = 0
+    var previousCalculatedMultiple = 0
+  
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var multiplesTextField: UITextField!
+    
+    @IBOutlet weak var multiplesLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onPlayButtonPressed(sender: UIButton) {
+        if multiplesTextField.text != nil && multiplesTextField.text != "" {
+            playButton.hidden = true
+            logoImage.hidden = true
+            multiplesTextField.hidden = true
+            
+            multiplesLabel.hidden = false
+            addButton.hidden = false
+            
+            userInputAmount = Int(multiplesTextField.text!)!
+           
+
+
+        }
+    }
+    
+    @IBAction func onAddButtonPressed (sender: UIButton) {
+            calculatedMultiple = calculatedMultiple + userInputAmount
+
+            updateMultiplesLabel()
+    }
+    
+    func updateMultiplesLabel(){
+        multiplesLabel.text = "\(calculatedMultiple - userInputAmount) + \(userInputAmount) = \(calculatedMultiple)"
+    }
+    
 
 }
 
